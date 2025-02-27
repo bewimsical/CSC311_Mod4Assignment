e1479d4e44cc135a92ddeda7dd8c8c533f3376f6
Kathleen Wims <bewimsical@gmail.com>
2025-02-26 20:04:13 -0500

    created Day Record parsed weather data into a list of Day records added a printWeather method

---
+++ src/main/java/edu/farmingdale/Day.java
@@ -0,0 +1,4 @@
+package edu.farmingdale;
+
+public record Day(String date, double temperature, double humidity, double precipitation) {
+}
--- src/main/java/edu/farmingdale/Main.java
+++ src/main/java/edu/farmingdale/Main.java
@@ -1,7 +1,47 @@
 package edu.farmingdale;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<Day> weather = new ArrayList<>();
+        FileReader fr;
+        try {
+            fr = new FileReader("weatherdata.csv");
+            Scanner sc = new Scanner(fr);
+            String[] headers_array = sc.nextLine().split(",");
+            Map<String, Integer> headers = new HashMap<>();
+            for (int i = 0; i < headers_array.length; i++) {
+                headers.put(headers_array[i].toLowerCase(), i);
+            }
+            while(sc.hasNextLine()){
+                String[] data = sc.nextLine().split(",");
+                String date = data[headers.get("date")];
+                double temp = Double.parseDouble(data[headers.get("temperature")]);
+                double hum = Double.parseDouble(data[headers.get("humidity")]);
+                double precip = Double.parseDouble(data[headers.get("precipitation")]);
+                Day d = new Day(date, temp, hum, precip);
+                weather.add(d);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e);
+        }
+
+        for(Day d: weather){
+            printWeather(d);
+        }
+    }
+    public static void printWeather(Day d){
+        String text ="""
+                    Here is the weather for: %s
+                        Temperature: %s degrees Fahrenheit
+                        Humidity: %s
+                        Precipitation: %s in
+                    """;
+
+        System.out.printf((text) + "%n", d.date(), String.valueOf(d.temperature()), String.valueOf(d.humidity()), String.valueOf(d.precipitation()));
     }
 }
